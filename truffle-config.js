f2216abc8cfd7262c759f7bca0647f643e7d0d04
RiccardoBiosas <rbios@protonmail.com>
2021-04-05 19:38:50 +0200

    truffle config changes and installed uuid library

--- truffle-config.js
+++ truffle-config.js
@@ -23,6 +23,11 @@ module.exports = {
       port: 8545,
       network_id: 80001
     },
+    bscTestnetFork: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: 97
+    },
     ropsten: {
       provider: () => new HDWalletProvider(process.env.MNEMONIC, process.env.INFURA_ROPSTEN),      
       network_id: 3
