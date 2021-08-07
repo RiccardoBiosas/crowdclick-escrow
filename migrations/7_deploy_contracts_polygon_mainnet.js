db295d7078e09d32ff35d1f9eb22af55778dfa85
RiccardoBiosas <rbios@protonmail.com>
2021-08-07 10:37:27 +0200

    added polygon mainnet migration

---
+++ migrations/7_deploy_contracts_polygon_mainnet.js
@@ -0,0 +1,31 @@
+const CrowdclickEscrow = artifacts.require('CrowdclickEscrow');
+const CrowdclickMockOracle = artifacts.require('CrowdclickMockOracle');
+
+const currencyApi = require('../dao/api');
+const config = require('../dao/environment');
+const { toE18 } = require('../dao/helpers');
+
+const { feePercentage } = config.contractDeployment.crowdclickEscrow;
+
+const deployCrowdclickMockOracle = async (deployer, owner) => {
+  const currentUnderlying = await currencyApi.fetchMaticToUSD;
+  const currentUnderlyingToWei = toE18(currentUnderlying.toString());
+  console.log(`current bnb price: ${currentUnderlying}`);
+  const crowdclickMockOracle = await deployer.deploy(CrowdclickMockOracle, currentUnderlyingToWei, owner, { from: owner });
+  return crowdclickMockOracle;
+};
+
+module.exports = function (deployer, network, accounts) {
+  deployer.then(async () => {
+    if (config.networkEnvironment === config.NETWORK_ENVIRONMENT.MATIC_MAINNET && config.isProduction) {
+      const owner = accounts[0];
+      console.log(`owner: ${owner}`)
+      const crowdclickMockOracle = await deployCrowdclickMockOracle(deployer, owner);
+      const crowdclickEscrow = await deployer.deploy(CrowdclickEscrow, crowdclickMockOracle.address, feePercentage, owner, { from: owner });
+      console.log(`CrowdclickMockOracle address: ${crowdclickMockOracle.address}`);
+      console.log(`crowdclickEscrow address: ${crowdclickEscrow.address}`);
+    }
+  }).catch((e) => {
+    console.error(e);
+  });
+};
